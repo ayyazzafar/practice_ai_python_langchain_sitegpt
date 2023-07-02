aa4e5ae724a23bf9dbd15a181084de4b9b466102
ayyazzafar <contact@ayyaztech.com>
2023-07-02 15:35:23 +0500

    files

--- streamlitui.py
+++ streamlitui.py
@@ -22,12 +22,14 @@ def process_input():
 
         st.session_state["messages"].append((user_text, True))
         st.session_state["messages"].append((query_text, False))
-        
+
+
 def ingest_input():
     if st.session_state["input_url"] and len(st.session_state["input_url"].strip()) > 0:
         url = st.session_state["input_url"].strip()
         with st.session_state["thinking_spinner"], st.spinner(f"Thinking"):
-            ingest_text = st.session_state["webquery"].ingest(url)        
+            ingest_text = st.session_state["webquery"].ingest(url)
+
 
 def is_openai_api_key_set() -> bool:
     return len(st.session_state["OPENAI_API_KEY"]) > 0
@@ -37,9 +39,11 @@ def main():
     if len(st.session_state) == 0:
         st.session_state["messages"] = []
         st.session_state["url"] = ""
-        st.session_state["OPENAI_API_KEY"] = os.environ.get("OPENAI_API_KEY", "")
+        st.session_state["OPENAI_API_KEY"] = os.environ.get(
+            "OPENAI_API_KEY", "")
         if is_openai_api_key_set():
-            st.session_state["webquery"] = WebQuery(st.session_state["OPENAI_API_KEY"])
+            st.session_state["webquery"] = WebQuery(
+                st.session_state["OPENAI_API_KEY"])
         else:
             st.session_state["webquery"] = None
 
@@ -54,18 +58,22 @@ def main():
             st.session_state["messages"] = []
             st.session_state["user_input"] = ""
             st.session_state["input_url"] = ""
-            st.session_state["webquery"] = WebQuery(st.session_state["OPENAI_API_KEY"])
+            st.session_state["webquery"] = WebQuery(
+                st.session_state["OPENAI_API_KEY"])
 
     st.subheader("Add a url")
-    st.text_input("Input url", value=st.session_state["url"], key="input_url", disabled=not is_openai_api_key_set(), on_change=ingest_input)
+    st.text_input("Input url", value=st.session_state["url"], key="input_url",
+                  disabled=not is_openai_api_key_set(), on_change=ingest_input)
 
     st.session_state["ingestion_spinner"] = st.empty()
 
     display_messages()
-    st.text_input("Message", key="user_input", disabled=not is_openai_api_key_set(), on_change=process_input)
+    st.text_input("Message", key="user_input",
+                  disabled=not is_openai_api_key_set(), on_change=process_input)
 
     st.divider()
-    st.markdown("Source code: [Github](https://github.com/Anil-matcha/Website-to-Chatbot)")
+    st.markdown(
+        "Source code: [Github](https://github.com/ayyazzafar/practice_ai_python_langchain_sitegpt)")
 
 
 if __name__ == "__main__":
